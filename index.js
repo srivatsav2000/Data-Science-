2453c28378432f0a2f7314c946b18293c8994620
srivatsav2000 <142770430+srivatsav2000@users.noreply.github.com>
2023-08-21 11:12:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const date = require("../date.js");
+async function validateRequestBody (req, res, next) {
+    const requestBody = req.body;
 
-var items = []; // Variables serving as local database instead of an actual database
-var workítems = [];
-
-async function getTodoList (req, res) {
-    const today = date.getDate();
-    const pageData = {
-        listTitle: today,
-        newListItems: items
-      };
-
-  res.render("list", pageData); //render from the list of view folder
-}
-
-async function createTodoItem (req, res) {
-    const item = req.body.newItem;
-    if (req.body.list && req.body.list === "Work") {
-        workítems.push(item);
-        res.redirect("/work");
+    if (!requestBody.newItem) {
+        return res.status(400).send(`<h1>A TODO List item was not found in the request body</h1>`);
     } else {
-        items.push(item);
-        res.redirect("/");
+        next(); // Pass the execution to the controllers
     }
-
 }
 
-async function getWorkList (req, res) {
-    const pageData = {
-        listTitle: "Work List",
-        newListItems: workítems
-    };
-
-    res.render("list", pageData);
-}
-
-module.exports = {getTodoList, getWorkList, createTodoItem};
+module.exports = {validateRequestBody};
